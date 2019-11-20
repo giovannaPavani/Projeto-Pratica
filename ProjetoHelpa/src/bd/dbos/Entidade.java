b635b43dab874316f4db1051dc870c1cb28a0b06
giovannaPavani <50486607+giovannaPavani@users.noreply.github.com>
2019-11-20 14:24:11 -0300

    commit da vida longa

---
+++ ProjetoHelpa/src/bd/dbos/Entidade.java
@@ -0,0 +1,326 @@
+package bd.dbos;
+
+public class Entidade implements Cloneable, Comparable<Entidade>
+{
+	private int codigo;
+	 private String nome;
+	 private String email;
+	 private String cnpj;
+	 private String telefone;
+	 private String conta;
+	 private String agencia;
+	 private String endereco;
+	 private String usuario;
+	 private String senha;
+	 private int visualizacoes;
+	 private String descricao;
+	 private String site;
+	 private String imagem;
+	
+	 public void setCodigo(int codigo) throws Exception
+	 {
+	    if (codigo <= 0)
+	       throw new Exception("Codigo invalido");
+	
+	       this.codigo = codigo;
+	 }  
+	 public void setVisualizacoes(int visualizacoes) throws Exception
+	 {
+	    if (codigo < 0)
+	       throw new Exception("Codigo invalido");
+	
+	       this.visualizacoes = visualizacoes;
+	 }  
+	 
+	 public void setNome(String nome) throws Exception
+	 {
+	     if (nome==null || nome.equals(""))
+	         throw new Exception ("Nome não fornecido");
+	
+	     this.nome = nome;
+	 }
+	
+	 public void setEmail(String email) throws Exception
+	 {
+	     if (email==null || email.equals(""))
+	         throw new Exception ("Email não fornecido");
+	
+	     this.email = email;
+	 }
+	
+	 public void setCnpj(String cnpj) throws Exception
+	 {
+	     if (cnpj==null || cnpj.equals(""))
+	         throw new Exception ("CNPJ não fornecido");
+	
+	     this.cnpj = cnpj;
+	 }
+	
+	 public void setConta(String conta) throws Exception
+	 {
+	     if (conta==null || conta.equals(""))
+	         throw new Exception ("Conta não fornecida");
+	
+	     this.conta = conta;
+	 }
+	
+	 public void setAgencia(String agencia) throws Exception
+	 {
+	     if (agencia==null || agencia.equals(""))
+	         throw new Exception ("Agência não fornecida");
+	
+	     this.agencia = agencia;
+	 }
+	
+	 public void setEndereco(String endereco) throws Exception
+	 {
+	     if (endereco==null || endereco.equals(""))
+	         throw new Exception ("Endereço não fornecido");
+	
+	     this.endereco = endereco;
+	 }
+	
+	 public void setUsuario(String usuario) throws Exception
+	 {
+	     if (usuario==null || usuario.equals(""))
+	         throw new Exception ("Usuário não fornecido");
+	
+	     this.usuario = usuario;
+	 }
+	
+	 public void setSenha(String senha) throws Exception
+	 {
+	     if (senha==null || senha.equals(""))
+	         throw new Exception ("Senha não fornecida");
+	
+	     this.senha = senha;
+	 }
+	 
+	 public void setTelefone(String telefone) throws Exception
+	 {
+	     if (telefone==null || telefone.equals("")) // formatação
+	         throw new Exception ("Telefone não fornecido");
+	
+	     this.telefone = telefone;
+	 }
+	 
+	 public void setSite(String site) throws Exception
+	 {
+	     if (site==null || site.equals(""))
+	         throw new Exception ("Site não fornecido");
+	
+	     this.site = site;
+	 }
+	 
+	 public void setDescricao(String desc) throws Exception
+	 {
+	     if (desc==null || desc.equals(""))
+	         throw new Exception ("Descrição não fornecida");
+	
+	     this.descricao = desc;
+	 }
+	
+	 public void setImagem(String img) throws Exception
+	 {
+	     if (img==null || img.equals(""))
+	         throw new Exception ("Nome não fornecido");
+	
+	     this.imagem = img;
+	 }
+	 
+	 public String getImagem ()
+	 {
+	     return this.imagem;
+	 }
+	 
+	 public int getCodigo ()
+	 {
+	     return this.codigo;
+	 }
+	
+	 public String getNome ()
+	 {
+	     return this.nome;
+	 }
+	
+	 public int getVisualizacoes ()
+	 {
+	     return this.visualizacoes;
+	 }
+	
+	 public String getEmail ()
+	 {
+	     return this.email;
+	 }
+	 
+	 public String getTelefone ()
+	 {
+	     return this.telefone;
+	 }
+	
+	 public String getCnpj ()
+	 {
+	     return this.cnpj;
+	 }
+	
+	 public String getConta ()
+	 {
+	     return this.conta;
+	 }
+	
+	 public String getAgencia ()
+	 {
+	     return this.agencia;
+	 }
+	
+	 public String getEndereco ()
+	 {
+	     return this.endereco;
+	 }
+	
+	 public String getUsuario ()
+	 {
+	     return this.usuario;
+	 }
+	
+	 public String getSenha ()
+	 {
+	     return this.senha;
+	 }
+	 
+	 public String getSite ()
+	 {
+	     return this.site;
+	 }
+	 
+	 public String getDescricao ()
+	 {
+	     return this.descricao;
+	 }
+	 
+	 public Entidade(int codigo) throws Exception
+	 {
+	    this.setCodigo  (codigo);
+	 }
+	
+	 public Entidade(int codigo, String nome, String email, String cnpj, String conta, String agencia, String endereco, String usuario, String senha, String telefone, int visualizacoes, String descricao, String site) throws Exception
+	 {
+	    this.setCodigo       (codigo);
+	    this.setNome         (nome);
+	    this.setEmail        (email);
+	    this.setConta        (conta);
+	    this.setAgencia      (agencia);
+	    this.setEndereco     (endereco);
+	    this.setUsuario      (usuario);
+	    this.setSenha        (senha);
+	    this.setCnpj         (cnpj);
+	    this.setVisualizacoes(visualizacoes);
+	    this.setSite         (site);
+	    this.setDescricao    (descricao);
+	 }
+
+	 public String toString ()
+	 {
+	     String ret="";
+	
+		 ret+="Codigo: "+this.codigo+"\n";
+		 ret+="Nome: "+this.nome  +"\n";
+		 ret+="Email: "+this.email + "\n";
+		 ret+= "CPNJ: " + this.cnpj +"\n";
+		 ret+="Conta: "+this.conta + "\n";
+		 ret+="Agência: "+this.agencia + "\n";
+		 ret+="Endereço: "+this.endereco + "\n";
+		 ret+="Usuário: "+this.usuario + "\n";
+		 ret+="Senha: "+this.senha + "\n";
+		 ret+="Telefone: "+this.telefone +"\n";
+		 ret+="Visualizacoes" + this.visualizacoes + "\n";
+		 ret+="Descrição: "+this.descricao + "\n";
+		 ret+="Site: "+this.site + "\n";
+		 
+		 return ret;
+	 }
+	
+	public int hashCode()
+	{
+		int ret = 1;
+		ret = ret * 5 + new Integer (this.codigo).hashCode();
+		ret = ret * 5 + this.nome.hashCode();
+		ret = ret * 5 + this.email.hashCode();
+		ret = ret * 5 + this.cnpj.hashCode();
+		ret = ret * 5 + this.telefone.hashCode();
+		ret = ret * 5 + this.conta.hashCode();
+		ret = ret * 5 + this.agencia.hashCode();
+		ret = ret * 5 + this.endereco.hashCode();
+		ret = ret * 5 + this.usuario.hashCode();
+		ret = ret * 5 + this.senha.hashCode();
+		ret = ret * 5 + this.site.hashCode();
+		ret = ret * 5 + this.descricao.hashCode();
+		ret = ret * 5 + new Integer (this.visualizacoes).hashCode();
+
+		if(ret < 0)
+			ret = -ret;
+		
+		return ret;
+	}
+
+	public boolean Equals(Object obj)
+	{
+		if(obj == this)
+			return true;
+		
+		if(obj == null)
+			return false;
+		
+		if(this.getClass() != obj.getClass())
+			return false;
+		
+		Entidade ent = (Entidade)obj;
+	
+		if(ent.codigo != this.codigo || ent.nome != this.nome || ent.email != this.email || ent.cnpj != this.cnpj || ent.telefone != this.telefone || ent.conta != this.conta || ent.agencia != this.agencia || ent.endereco != this.endereco || ent.usuario != this.usuario || ent.senha != this.senha || ent.visualizacoes != this.visualizacoes || ent.site != this.site || ent.descricao != this.descricao)
+			return false;
+	
+		return true;
+	}
+	
+	public Object clone()
+	{
+		Entidade ret = null;
+		try
+		{
+			ret = new Entidade (this);
+		}
+		catch(Exception erro)
+		{} 
+		return ret;
+	}
+	
+	public Entidade(Entidade molde) throws Exception
+	{
+		if(molde == null)
+			throw new Exception();
+		
+		this.codigo = molde.codigo;
+		this.nome = molde.nome;
+		this.email = molde.email;
+		this.cnpj = molde.cnpj;
+		this.telefone = molde.telefone;
+		this.conta = molde.conta;
+		this.agencia = molde.agencia;
+		this.endereco = molde.endereco;
+		this.usuario = molde.usuario;
+		this.senha = molde.senha;
+		this.visualizacoes = molde.visualizacoes;
+		this.site = molde.site;
+		this.descricao = molde.descricao;
+	}
+	
+	public int compareTo(Entidade outro)
+	{
+		// n tem throw exception???
+		if(this.codigo>outro.codigo)
+			return 1;
+		if(this.codigo<outro.codigo)
+			return -1;
+		return 0;
+	}
+}
---
+++ ProjetoHelpa/src/views/PrincipalFunc.java
@@ -0,0 +1,386 @@
+package views;
+import bd.daos.*;
+import bd.dbos.*;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.Font;
+import java.awt.List;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class PrincipalFunc extends JFrame {
+	
+	private JFrame frame;
+	private JPanel contentPane;
+	Funcionario funcAtual;
+	boolean alterou = false;
+
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrincipalFunc frame = new PrincipalFunc();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the frame.
+	 */
+	int codigo;
+	private JTextField txtNome;
+	private JTextField txtEmail;
+	private JTextField txtTelefone;
+	private JTextField txtEndereco;
+	private JTextField txtUsuario;
+	private JButton btnSalvar;
+	JButton btnCancelar;
+	
+	public PrincipalFunc(int cod) {
+		codigo = cod;
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		try
+		{
+		  funcAtual = new Funcionario(Funcionarios.getFuncionarioByCod(codigo));
+		}
+		catch(Exception ex) {} // n vai dar erro pois o codigo foi eu que passei
+		
+		frame = new JFrame();
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				if(alterou)
+				{
+					int choice = JOptionPane.showOptionDialog(null, 
+						      "Deseja sair e salvar?", 
+						      "Sair", 
+						      JOptionPane.YES_NO_CANCEL_OPTION, 
+						      JOptionPane.QUESTION_MESSAGE, 
+						      null, null, null);
+
+						  if (choice == JOptionPane.YES_OPTION)
+						  {
+							  btnSalvar.doClick();
+						  }
+						  else if(choice == JOptionPane.NO_OPTION)
+						  {
+							  System.exit(0);
+						  }
+				}
+				else
+					System.exit(0);
+			}
+		});
+		frame.getContentPane().addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// ABRIR FORM DE MANUTENÇÃO DE ENTIDADES
+				ManutEntidades formME = new ManutEntidades();
+				formME.setVisible(true);
+			}
+		});
+		frame.setBounds(100, 100, 482, 341);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel label = new JLabel("Nome:");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label.setBounds(55, 53, 48, 32);
+		frame.getContentPane().add(label);
+		
+		JLabel label_2 = new JLabel("E-mail:");
+		label_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label_2.setBounds(50, 127, 53, 23);
+		frame.getContentPane().add(label_2);
+		
+		JLabel label_3 = new JLabel("Telefone:");
+		label_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label_3.setBounds(37, 93, 73, 23);
+		frame.getContentPane().add(label_3);
+		
+		JButton btnAlterar = new JButton("Alterar");
+		btnAlterar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				txtNome.setEnabled(true);
+				txtEmail.setEnabled(true);
+				txtTelefone.setEnabled(true);
+				txtEndereco.setEnabled(true);
+				txtUsuario.setEnabled(true);
+				btnSalvar.setEnabled(true);
+				alterou = true;
+			}
+		});
+		btnAlterar.setForeground(Color.BLACK);
+		btnAlterar.setBounds(96, 224, 85, 32);
+		frame.getContentPane().add(btnAlterar);
+		
+		btnSalvar = new JButton("Salvar");
+		btnSalvar.setEnabled(false);
+		btnSalvar.setBounds(286, 224, 85, 32);
+		btnSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtNome.getText().equals("") ||
+				txtEmail.getText().equals("") ||
+				txtTelefone.getText().equals("") ||
+				txtEndereco.getText().equals("") ||
+				txtUsuario.getText().equals(""))
+				{
+					JOptionPane.showMessageDialog(null,"Não deixe nenhum campo em branco!");
+					btnCancelar.doClick();
+				}
+				else
+				{
+					try
+					{
+						funcAtual.setEndereco(txtEndereco.getText());
+						funcAtual.setUsuario(txtUsuario.getText());
+						funcAtual.setEmail(txtEmail.getText());
+						funcAtual.setNome(txtNome.getText());
+						funcAtual.setTelefone(txtTelefone.getText());
+						Funcionarios.alterar(funcAtual);
+						alterou = false;
+						JOptionPane.showMessageDialog(null,"Alteração feita com sucesso!");
+					}
+					catch(Exception ex) {/*fazer*/}
+				}
+				btnSalvar.setEnabled(false);
+				txtNome.setEnabled(false);
+				txtEmail.setEnabled(false);
+				txtTelefone.setEnabled(false);
+				txtEndereco.setEnabled(false);
+				txtUsuario.setEnabled(false);
+			}
+		});
+		frame.getContentPane().add(btnSalvar);
+		
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtEmail.setText(funcAtual.getEmail());
+				txtNome.setText(funcAtual.getNome());
+				txtEndereco.setText(funcAtual.getEndereco());
+				txtUsuario.setText(funcAtual.getUsuario());
+				txtTelefone.setText(funcAtual.getTelefone());
+				btnSalvar.setEnabled(false);
+				txtNome.setEnabled(false);
+				txtEmail.setEnabled(false);
+				txtTelefone.setEnabled(false);
+				txtEndereco.setEnabled(false);
+				txtUsuario.setEnabled(false);
+			}
+		});
+		btnCancelar.setForeground(Color.BLACK);
+		btnCancelar.setBounds(191, 224, 85, 32);
+		frame.getContentPane().add(btnCancelar);
+		
+		txtNome = new JTextField();
+		txtNome.setEnabled(false);
+		txtNome.setBounds(119, 62, 298, 23);
+		frame.getContentPane().add(txtNome);
+		txtNome.setColumns(10);
+		txtNome.setText(funcAtual.getNome());
+		
+		txtEmail = new JTextField();
+		txtEmail.setEnabled(false);
+		txtEmail.setColumns(10);
+		txtEmail.setBounds(119, 129, 298, 23);
+		frame.getContentPane().add(txtEmail);
+		txtEmail.setText(funcAtual.getEmail());
+		
+		txtTelefone = new JTextField();
+		txtTelefone.setEnabled(false);
+		txtTelefone.setColumns(10);
+		txtTelefone.setBounds(119, 95, 298, 23);
+		frame.getContentPane().add(txtTelefone);
+		txtTelefone.setText(funcAtual.getTelefone());
+		
+		JLabel lblEndereo = new JLabel("Endere\u00E7o:");
+		lblEndereo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblEndereo.setBounds(30, 161, 73, 23);
+		frame.getContentPane().add(lblEndereo);
+		
+		txtEndereco = new JTextField();
+		txtEndereco.setEnabled(false);
+		txtEndereco.setColumns(10);
+		txtEndereco.setBounds(119, 161, 298, 23);
+		frame.getContentPane().add(txtEndereco);
+		txtEndereco.setText(funcAtual.getEndereco());
+		
+		JLabel lblUsurio = new JLabel("Usu\u00E1rio:");
+		lblUsurio.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblUsurio.setBounds(43, 195, 60, 23);
+		frame.getContentPane().add(lblUsurio);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setEnabled(false);
+		txtUsuario.setColumns(10);
+		txtUsuario.setBounds(119, 194, 200, 23);
+		frame.getContentPane().add(txtUsuario);
+		txtUsuario.setText(funcAtual.getUsuario());
+		
+		JButton btnTrocarSenha = new JButton("Trocar senha");
+		btnTrocarSenha.setForeground(Color.BLACK);
+		btnTrocarSenha.setBounds(322, 189, 95, 32);
+		frame.getContentPane().add(btnTrocarSenha);
+		
+		JLabel lblHelpa = new JLabel(" \u00C1rea do funcion\u00E1rio");
+		lblHelpa.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblHelpa.setBounds(159, 21, 190, 37);
+		frame.getContentPane().add(lblHelpa);
+		/*JMenu menManutencao;
+		menManutencao = new JMenu("MANUTEN\u00C7\u00C3O");
+		menManutencao.setBounds(55, 270, 131, 22);
+		frame.getContentPane().add(menManutencao);
+		menManutencao.setHorizontalAlignment(SwingConstants.CENTER);
+		*/
+		JMenuBar menuBar = new JMenuBar();
+		JMenu ManutMenu = new JMenu("MANUTENÇÃO");
+        JMenu RelatMenu = new JMenu("CONSULTAS");
+        menuBar.add(ManutMenu);
+        menuBar.add(RelatMenu);
+        
+        JMenuItem ManutFunc = new JMenuItem("Funcionários");
+        ManutFunc.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// abrir form de manutencao de funcionarios
+			}
+		});
+        JMenuItem ManutEnt = new JMenuItem("Entidades");
+        ManutEnt.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// abrir form de manutencao de entidades
+			}
+		});
+        JMenuItem ManutDoa = new JMenuItem("Doadores");
+        ManutDoa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// abrir form de manutencao de doadores
+			}
+		});
+        
+        JMenuItem RelatEnt = new JMenuItem("Entidades");
+        RelatEnt.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// abrir form de relatorio de entidades
+			}
+		});
+        JMenuItem RelatDoa = new JMenuItem("Doadores");
+        RelatDoa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// abrir form de relatorio de entidades
+			}
+		});
+         
+        ManutMenu.add(ManutFunc);
+        ManutMenu.add(ManutEnt);
+        ManutMenu.add(ManutDoa);
+        
+        RelatMenu.add(RelatEnt);
+        RelatMenu.add(RelatDoa);
+        
+        
+        menuBar.setBounds(100, 260, 200, 40);
+        frame.getContentPane().add(menuBar);
+
+		/*JMenuItem miManutEntidades = new JMenuItem("ENTIDADES");
+		miManutEntidades.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		menManutencao.add(miManutEntidades);
+		
+		
+		JMenuItem miManutFuncs = new JMenuItem("FUNCION\u00C1RIOS");
+		miManutFuncs.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// ABRIR FORM DE MANUTENÇÃO DE FUNCIONÁRIOS
+				ManutFuncs formMF = new ManutFuncs();
+				formMF.setVisible(true);
+			}
+		});
+		menManutencao.add(miManutFuncs);
+		
+		JMenu menConsultas = new JMenu("CONSULTAS");
+		menConsultas.setBounds(273, 270, 131, 22);
+		frame.getContentPane().add(menConsultas);
+		
+		JMenuItem miConsuEntidades = new JMenuItem("ENTIDADES");
+		miConsuEntidades.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// ABRIR P/ CONSULTA ENTIDADES
+				ConsultaEntidades formCE = new ConsultaEntidades();
+				formCE.setVisible(true);
+			}
+		});
+		menConsultas.add(miConsuEntidades);
+		
+		JMenuItem miConsuDoadores = new JMenuItem("DOADORES");
+		miConsuDoadores.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// ABRIR P/ CONSULTA DOAD
+				ConsultaDoadores formCD = new ConsultaDoadores();
+				formCD.setVisible(true);
+			}
+		});
+		menConsultas.add(miConsuDoadores);
+		
+		JMenuItem miConsuFuncs = new JMenuItem("DOA\u00C7\u00D5ES");
+		miConsuFuncs.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			// ABRIR FORM P/ CONSULTA FUNCIONÁRIOS
+				ConsultaFuncs formCF = new ConsultaFuncs();
+				formCF.setVisible(true);
+			}
+		});
+		menConsultas.add(miConsuFuncs);*/
+		
+		JLabel lblNewLabel = new JLabel("HELPA!");
+		lblNewLabel.setBounds(216, 11, 46, 14);
+		frame.getContentPane().add(lblNewLabel);
+	}
+}
