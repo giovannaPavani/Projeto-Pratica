6fa88fa8358ce8e6d174b853ef182a5833c27fe7
malusperancin <50486625+malusperancin@users.noreply.github.com>
2019-11-25 17:02:48 -0300

    mais 1

--- AplicativoHelpa/src/bd/dbos/Doacao.java
+++ AplicativoHelpa/src/bd/dbos/Doacao.java
@@ -1,72 +1,122 @@
 package bd.dbos;
 import java.util.Date;
-
+/**
+	A classe Doacao é uma classe feita para armazenar dados de
+	doação (que é uma tabela do banco de dados).
+	Nela encontramos setters e getters, além do construtor e métodos obrigatórios.
+	@author Giovanna Pavani Martelli - 19173 e Maria Luiza Sperancin Mancebo - 19186
+	@since 2019.
+*/
 public class Doacao implements Cloneable, Comparable<Doacao>
 {
+	/**Armazena o id da doacao*/
 	private int id;
+	/**Armazena o codigo da pessoa que realizou a doação*/
     private int codPessoa;
+    /**Armazena o produto que foi doado*/
     private String produto;
+   /**Armazena o codigo da entidade que recebeu a doação*/
     private int codEntidade;
+   /**Armazena a data da doação*/
     private Date data;
+    /**Armazena se ela foi entregue ou não*/
     private char entregue;
+    /**Armazena a quantidade da doação*/
     private String quantidade;
 
+
+/**
+	  Atribui valor ao ID
+	  Atribui ao atributo id um número inteiro passado por parâmetro.
+	  @param o numero que será o id
+	  @throws Exception caso o id for menor que zero
+	 */
   public void setId (int id) throws Exception
   {
       if (id <= 0)
           throw new Exception ("Codigo invalido");
 
       this.id = id;
-  } 
-  
+  }
+
+/**
+	  Atribui valor ao codigo da pessoa
+	  Atribui ao atributo codPessoa um número inteiro passado por parâmetro.
+	  @param o numero que será o codigo da pessoa
+	  @throws Exception caso o cod for menor que zero
+	 */
   public void setCodPessoa (int cod) throws Exception
   {
       if (cod <= 0)
           throw new Exception ("Codigo pessoa invalido");
 
       this.codPessoa = cod;
-  } 
-  
+  }
+
+/**
+	  Atribui valor ao produto
+	  Atribui ao atributo protudo uma String passada por parâmetro.
+	  @param A string q será atribuida
+	  @throws Exception caso seja null ou vazio
+	 */
   public void setProduto (String prod) throws Exception
   {
       if (prod == null || prod.equals(""))
           throw new Exception ("Data invalida");
 
       this.produto = prod;
-  } 
-  
+  }
+
+/**
+	  Atribui valor ao codigo da entidade
+	  Atribui ao atributo codEntidade um número inteiro passado por parâmetro.
+	  @param o numero que será o codigo da entidade
+	  @throws Exception caso o cod for menor que zero
+	 */
   public void setCodEntidade (int cod) throws Exception
   {
       if (cod <= 0)
           throw new Exception ("Codigo entidade invalido");
 
       this.codEntidade = cod;
-  } 
-  
+  }
+
+/**
+	  Atribui valor a data
+	  Atribui ao atributo data uma Data passado por parâmetro.
+	  @param a data que será atribuida
+	  @throws Exception caso a data for nula
+	 */
   public void setData (Date dat) throws Exception
   {
       if (dat == null)
           throw new Exception ("Data invalida");
 
       this.data = dat;
-  } 
-  
+  }
+
+/**
+	  Atribui valor ao codigo da pessoa
+	  Atribui ao atributo codPessoa um número inteiro passado por parâmetro.
+	  @param o numero que será o codigo da pessoa
+	  @throws Exception caso o cod for menor que zero
+	 */
   public void setEntregue (char entreg) throws Exception
   {
       if (entreg!='S' && entreg!='N')
           throw new Exception ("Campo 'Entregue?' inválido");
 
       this.entregue = entreg;
-  } 
-  
+  }
+
   public void setQuantidade (String qtd) throws Exception
   {
       if (qtd == null || qtd.equals(""))
           throw new Exception ("Data invalida");
 
       this.quantidade = qtd;
-  } 
-  
+  }
+
   public String getProduto ()
   {
          return this.produto;
@@ -76,33 +126,33 @@ public class Doacao implements Cloneable, Comparable<Doacao>
  {
         return this.id;
  }
- 
+
  public int getCodEntidade ()
  {
      return this.codEntidade;
  }
- 
+
  public int getCodPessoa ()
  {
      return this.codPessoa;
  }
- 
- public Date getData() 
+
+ public Date getData()
  {
 	 return this.data;
  }
- 
+
  public char getEntregue ()
  {
 	 return this.entregue;
  }
- 
+
  public String getQuantidade ()
  {
         return this.quantidade;
  }
-	 
-	
+
+
 	public Doacao (int id, int codPes, String produto, int codEnt, Date dat, char entreg, String qtd)throws Exception
 	{
 		this.setId          (id);
@@ -113,11 +163,11 @@ public class Doacao implements Cloneable, Comparable<Doacao>
 	    this.setEntregue    (entreg);
 	    this.setQuantidade  (qtd);
 	}
-	
+
 	public String toString ()
     {
         String ret="";
-        
+
         ret+="Id: "+this.id+"\n";
         ret+="CodPessoa: "+this.codPessoa  +"\n";
 	    ret+="CodEntidade: "+this.codEntidade  +"\n";
@@ -126,25 +176,25 @@ public class Doacao implements Cloneable, Comparable<Doacao>
 	    ret+="Quantidade: " + this.quantidade;
         return ret;
     }
-	
+
 	public boolean Equals(Object obj)
 	{
 	  if(obj == null)
 	    return false;
-	  
+
 	  if(obj == this)
 	    return true;
-	  
+
 	  if(this.getClass() != obj.getClass())
 	    return false;
-	  
-	  Doacao  doa = (Doacao) obj; 
+
+	  Doacao  doa = (Doacao) obj;
 	  if(this.id != doa.id || this.codPessoa != doa.codPessoa ||this.codEntidade != doa.codEntidade || !(this.data.equals(doa.data)) || !(this.quantidade.equals(doa.quantidade)) || this.entregue != doa.entregue)
 	    return false;
-	  
+
 	  return true;
 	}
-	
+
 	public int hashCode()
 	{
 	  int ret = 1;
@@ -155,10 +205,10 @@ public class Doacao implements Cloneable, Comparable<Doacao>
 	  ret = ret * 5 + this.quantidade.hashCode();
 	  ret = ret * 5 + this.produto.hashCode();
 	  ret = ret * 5 + new Character (this.entregue).hashCode();
-	
+
 	  return ret;
 	}
-	
+
 	public Doacao(Doacao modelo)throws Exception
 	{
 	  if(modelo == null)
@@ -171,7 +221,7 @@ public class Doacao implements Cloneable, Comparable<Doacao>
 	  this.entregue = modelo.entregue;
 	  this.quantidade = modelo.quantidade;
 	}
-	
+
 	public Object clone()
 	{
 	  Doacao ret = null;
@@ -182,7 +232,7 @@ public class Doacao implements Cloneable, Comparable<Doacao>
 	  catch(Exception erro){}
 	  return ret;
 	}
-	
+
 	public int compareTo(Doacao outro)
 	{
 		if(this.id>outro.id)
